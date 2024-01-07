52e7aa3b8e4928c0a4f90ada280be4a056eab3be
sabri1997 <50168348+sabri1997@users.noreply.github.com>
2024-01-07 14:53:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(window).on("load", function () {
+//   // home section slidesh
+//   let slideIndex = $(".slide.active").index();
+//   const slideLen = $(".slide").length;
+
+//   function slideShow() {
+//     $(".slide").removeClass("active").eq(slideIndex).addClass("active");
+
+//     if (slideIndex == slideLen - 1) {
+//       slideIndex = 0;
+//     } else {
+//       slideIndex++;
+//     }
+//     setTimeout(slideShow, 5000);
+//   }
+//   slideShow();
+// });
