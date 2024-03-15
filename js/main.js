3046a991b85b7c68baafe8f1045bb6868b351ce6
sabri1997 <50168348+sabri1997@users.noreply.github.com>
2024-03-15 09:36:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,30 @@
 //   }
 //   slideShow();
 // });
+
+//nav toggle
+$(".hamburger-btn").click(function(){
+    $(".header .nav").slideToggle();
+})
+$(".header .nav a").click(function(){
+    if($(window).width() < 768){
+        $(".header .nav").slideToggle();
+    }
+})
+
+//fixed header
+$(window).scroll(function(){
+    if($(this).scrollTop > 100){
+        $(".header").addClass("fixed");
+    }
+    else {
+        $(".header").removeClass("fixed");
+    }
+})
+
+//scrollIT
+
+    $.scrollIt({
+        topOffset: 0
+    });
+ 
